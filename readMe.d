8fbf722c4a4fa53ffe185fbd7df498bd7559231a
jindalabhishek1 <32256963+jindalabhishek1@users.noreply.github.com>
2017-10-29 19:50:17 +0530

    create readme.md
    Created readMe.md

---
+++ readMe.d
@@ -0,0 +1,12 @@
+Introduction:
+
+This is a top level site for electrolysis.
+
+Licence information:
+
+GNU GENERAL PUBLIC LICENSE
+                       Version 3, 29 June 2007
+
+ Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ Everyone is permitted to copy and distribute verbatim copies
+ of this license document, but changing it is not allowed.
